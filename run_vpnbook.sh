0dfea3c47ea7aac4b46292451a13e14aba7505e7
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-01-12 19:15:31 +0530

    run_vpnbook.sh

--- run_vpnbook.sh
+++ run_vpnbook.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 while true; do
-  for file in *.sh; do
-    echo "Running $file"
-    bash "$file"
-    echo "Waiting 10 seconds..."
+  for file in *.ovpn; do
+    echo "Connecting using $file"
+    sudo openvpn --config "$file" &
+    VPN_PID=$!
+
     sleep 10
+
+    echo "Disconnecting $file"
+    sudo kill $VPN_PID
+    sleep 2
   done
 done
