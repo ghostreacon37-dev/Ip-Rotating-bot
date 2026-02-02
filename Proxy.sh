8812df4a912c3d88583c11cc199f892bf8d1add7
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-03 02:57:07 +0530

    Update Proxy.sh

--- Proxy.sh
+++ Proxy.sh
@@ -3,4 +3,4 @@ URL="https://free-proxy-list.net/en/"
 OUT="proxy.txt"
 
 #curl -s "$URL" | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk '{print $1 ":" $2}' | tee "$OUT"
-curl -s "https://free-proxy-list.net/en/" | awk -F'<td>|</td>' '/<tbody>/{f=1} f && /<tr>/{ip="";port=""} f && $2 ~ /^[0-9.]+$/ {ip=$2} f && $4 ~ /^[0-9]+$/ {port=$4; if(ip!="") print ip ":" port} /</tbody>/{f=0}'
+curl -s "https://free-proxy-list.net/en/" | awk '/<tbody>/{f=1} f; /</tbody>/{exit}' | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk 'NF==2{print $1 ":" $2}' | tee "proxies.txt"
