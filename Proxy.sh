f48c1a3ab0e70505c96b5b24dc964d5fd258c9d8
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-03 03:08:49 +0530

    Free-proxy to Proxy.txt (file)

--- Proxy.sh
+++ Proxy.sh
@@ -2,5 +2,4 @@
 URL="https://free-proxy-list.net/en/"
 OUT="proxy.txt"
 
-#curl -s "$URL" | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk '{print $1 ":" $2}' | tee "$OUT"
-curl -s "https://free-proxy-list.net/en/" | awk '/<tbody>/{f=1} f; /</tbody>/{exit}' | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk 'NF==2{print $1 ":" $2}' | tee "proxies.txt"
+curl -s "$URL" | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk '{print $1 ":" $2}' | tee "$OUT"
