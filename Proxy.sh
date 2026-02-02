1878e8e3b874e17e4479f39df237b4f9605a277e
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-03 02:49:09 +0530

    Free-proxy to Proxy.txt (file)

--- Proxy.sh
+++ Proxy.sh
@@ -2,4 +2,5 @@
 URL="https://free-proxy-list.net/en/"
 OUT="proxy.txt"
 
-curl -s "$URL" | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk '{print $1 ":" $2}' | tee "$OUT"
+#curl -s "$URL" | grep -oP '(?<=<td>)(\d{1,3}.){3}\d{1,3}(?=</td>)|(?<=</td><td>)\d{2,5}(?=</td>)' | paste - - | awk '{print $1 ":" $2}' | tee "$OUT"
+curl -s "https://free-proxy-list.net/en/" | awk -F'<td>|</td>' '/<tbody>/{f=1} f && /<tr>/{ip="";port=""} f && $2 ~ /^[0-9.]+$/ {ip=$2} f && $4 ~ /^[0-9]+$/ {port=$4; if(ip!="") print ip ":" port} /</tbody>/{f=0}'
