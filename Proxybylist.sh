832da91eca9b0887c52dbf0397f06fa0c6b46f09
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-03 04:07:00 +0530

    Proxybylist.sh

--- Proxybylist.sh
+++ Proxybylist.sh
@@ -28,14 +28,26 @@ rm -f dead.txt
 
 
 while read -r proxy; do
-  echo -n "Checking $proxy ... "
+    echo -n "Checking $proxy ... "
 
-  if curl -s --proxy "http://$proxy" --max-time 5 https://api.ipify.org >/dev/null 2>&1; then
-    echo "WORKING"
-    echo "$proxy" >> "$WORKING"
-  else
-    echo "DEAD"
-  fi
+    # First attempt with http
+    if curl -s --proxy "http://$proxy" --max-time 5 https://api.ipify.org >/dev/null 2>&1; then
+        echo "WORKING"
+        echo "$proxy" >> "$WORKING"
+    # Second attempt with socks4
+    elif curl -s --proxy "socks4://$proxy" --max-time 5 https://api.ipify.org >/dev/null 2>&1; then
+        echo "WORKING"
+        echo "$proxy" >> "$WORKING"
+    # Third attempt with socks5
+    elif curl -s --proxy "socks5://$proxy" --max-time 5 https://api.ipify.org >/dev/null 2>&1; then
+        echo "WORKING"
+        echo "$proxy" >> "$WORKING"
+    elif curl -s --proxy "https://$proxy" --max-time 5 https://api.ipify.org >/dev/null 2>&1; then
+        echo "WORKING"
+        echo "$proxy" >> "$WORKING"
+    else
+        echo "DEAD"
+    fi
 done < "$ALL"
 
 echo
