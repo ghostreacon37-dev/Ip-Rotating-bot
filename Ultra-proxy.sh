33ac7c388bd34516d7c8730aae7a83b2e52a0c31
ghostreacon37-dev <ghostreacon37@gmail.com>
2026-02-07 03:32:00 +0530

    run ./Ultra-proxy.sh

--- Ultra-proxy.sh
+++ Ultra-proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install gnome-terminal -y
-chmod +x Ip-taker.sh Proxy-changer.sh
+chmod +x Ip-taker.sh Proxy-changer.sh ip-taker.sh proxy-changer.sh
 gnome-terminal -- bash -c "./ip-taker.sh; exec bash"
 gnome-terminal -- bash -c "./proxy-changer.sh; exec bash"
